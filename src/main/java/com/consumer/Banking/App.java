80002ea60816f09a34cfa99a1de60876842fbd97
dev1 <dev1@gmail.com>
2024-03-22 11:23:19 +0530

    adding new consumer banking project to remote repo

---
+++ src/main/java/com/consumer/Banking/App.java
@@ -0,0 +1,13 @@
+package com.consumer.Banking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
